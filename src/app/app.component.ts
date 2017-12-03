5a1ca27874af61373111ad252ec7175a181f6a96
Natalia Kędziora <n.m.kedziora@gmail.com>
2017-12-03 12:22:41 +0100

    Creates hardcoded items list with mark done functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  items: any[] = [
+    { name: 'item1', done: false },
+    { name: 'item2', done: false },
+    { name: 'item3', done: false },
+    { name: 'item4', done: false }
+  ];
+
+  toggleDone(item) {
+    item.done = !item.done;
+    console.log('toggleDone', item);
+  }
+}
