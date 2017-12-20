897e164a38f6e5f1acd9f66fc8891fee87e52b91
Natalia Kędziora <n.m.kedziora@gmail.com>
2017-12-20 17:23:49 +0100

    Reads and writes shopping items to Firebase database

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,37 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  items: any[] = [
-    { name: 'item1', done: false },
-    { name: 'item2', done: false },
-    { name: 'item3', done: false },
-    { name: 'item4', done: false }
-  ];
+  items$: FirebaseListObservable<any[]>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.items$ = db.list('/');
+  }
 
   toggleDone(item: any) {
-    item.done = !item.done;
-
-    this.sortByDone();
+    this.db.object(`/${item.$key}`).update({
+      done: !item.done
+    });
   }
 
   add(input: HTMLInputElement) {
-    this.items.push({
+    this.items$.push({
       name: input.value,
       done: false
     });
+
     input.value = '';
-    this.sortByDone();
+    // this.sortByDone();
   }
 
   sortByDone() {
-    this.items.sort((a: any, b: any) :number => {
+    this.items$.sort((a: any, b: any) :number => {
       if(a.done && !b.done) return 1;
       if(!a.done && b.done) return -1;
       return 0; 
